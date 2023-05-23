b70ad20c0ee85e629064afa78ea3cefdf3306ecb
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-05-23 08:13:02 -0300

    criando conexao.php

---
+++ dao/conexao.php
@@ -0,0 +1,24 @@
+<?php
+
+function conectar()
+{
+    $dbHost = '127.0.0.1:3306';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = `bd-igreja`;
+
+    $conexao = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName) or die("Erro ao conectar: ");
+
+    // if($conexao->connect_errno)
+    // {
+    //     echo "Erro";
+    // }
+    // else
+    // {
+    //     echo "Conexão efetuada com sucesso";
+    // }
+
+    // return $conexao;
+}
+
+//conectar();

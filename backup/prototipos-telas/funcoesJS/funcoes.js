e58ee45594e409f0c6caa79b8e7eddf801545194
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-05-23 08:03:55 -0300

    Implementando a divisão de arquivos segundo o MVC

---
+++ backup/prototipos-telas/funcoesJS/funcoes.js
@@ -0,0 +1,16 @@
+function carregaDocumento(arquivo, target) {
+    var el = document.querySelector(target);
+
+    //Se o elemento não existir então não requisita
+    if (!el) return;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", arquivo, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 300) {
+            el.innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send(null);
+}

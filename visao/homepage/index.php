6ed7bcd3d22dfdbbfca3b78e21594644175c89c1
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-07-31 17:32:18 -0300

    inserção modal confirmação exclusão avisos

--- visao/homepage/index.php
+++ visao/homepage/index.php
@@ -29,7 +29,7 @@
     <!-- Código para linkar a navbar, que se encontra em arquivo separado -->
     <header id="header"></header>
 
-    <div class="container">
+    <div class="container" id="container">
 
         <div class="box__carrossel">
             <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
@@ -75,7 +75,7 @@
                     Evento</button>
             </div>
 
-            <!-- modal -->
+            <!-- ___________________________ modal de cadastro dos eventos _____________________________ -->
             <div class="modal fade" id="modal-eventos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
@@ -137,6 +137,7 @@
                     </div>
                 </div>
             </div>
+            <!-- _________________________________________________________________________________________ -->
 
             <div class="box__eventos-body">
                 <div class="box__data">
@@ -173,7 +174,7 @@
                         Aviso</button>
                 </div>
 
-                <!-- modal -->
+                <!-- _____________________ modal de cadastro dos avisos _____________________ -->
                 <div class="modal fade" id="modal-avisos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -217,6 +218,26 @@
                     </div>
                 </div>
 
+                <!-- _____________________ modal confirmação exclusão avisos  _____________________  -->
+                <div class="modal fade" id="modalExclusaoAviso" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Excluir Aviso</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Deseja excluir este aviso?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                                <button type="button" class="btn btn-primary" id="btnDeleteAvisoModal" value="" onclick="deleteAviso(this.value)">Excluir</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  __________________________________________________________________________________  -->
+
                 <div class="box__avisos-body">
                     <div class="list-group">
 
@@ -233,7 +254,7 @@
                             echo "<div class='d-flex w-100 justify-content-between'>";
                             echo "<h5 class='mb-1'><svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-arrow-right mb-1' viewBox='0 0 16 16'>";
                             echo "<path fill-rule='evenodd' d='M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z'/>";
-                            echo "</svg>" ;
+                            echo "</svg>";
                             echo " " . $user_data['titulo'];
                             echo "</h5>";
                             echo "<div class='buttonsAviso'>
@@ -242,8 +263,8 @@
                                                 <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/>
                                             </svg>
                                         </button>
-
-                                        <button class='btn btn-sm' onclick='deleteAviso($id_aviso)'>
+                                       
+                                        <button class='btn btn-sm' data-bs-toggle='modal' data-bs-target='#modalExclusaoAviso' onclick=setarIdModal($id_aviso)>
                                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-trash3-fill' viewBox='0 0 16 16'>
                                                 <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z' />
                                             </svg>
@@ -262,7 +283,7 @@
                                 <h5 class="mb-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right mb-1" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                    </svg> 
+                                    </svg>
                                     List group item heading
                                 </h5>
                                 <!-- <small class="text-secondary">3 days ago</small> -->
@@ -385,7 +406,11 @@
 </script>
 
 <script>
-    function deleteAviso(id){
+    function setarIdModal(id) {
+        document.getElementById('btnDeleteAvisoModal').value = id;
+    }
+
+    function deleteAviso(id) {
         window.location.href = "../../controlador/deletarAviso.php?id=" + id;
     }
 </script>

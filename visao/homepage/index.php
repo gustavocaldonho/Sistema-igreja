6fd269de3149f924dbf4ba58529761d124e2423f
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-08-01 21:18:16 -0300

    adicionando small status avisos

--- visao/homepage/index.php
+++ visao/homepage/index.php
@@ -261,8 +261,16 @@
                             echo "</svg>";
                             echo " " . $titulo;
                             echo "</h5>";
+
                             echo "<div class='buttonsAviso'>
-                                        <button class='btn btn-sm' data-bs-toggle='modal' data-bs-target='#modal-avisos' onclick='setarModalAvisoUpdate(\"$titulo\", \"$descricao\", $status, $id_aviso)'>
+                                        <small class='small-status";
+                            if ($status == 0) {
+                                echo " comunidade'>Comunidade</small>";
+                            } else {
+                                echo " paroquia'>Paróquia</small>";
+                            }
+
+                            echo "<button class='btn btn-sm' data-bs-toggle='modal'                 data-bs-target='#modal-avisos' onclick='setarModalAvisoUpdate(\"$titulo\", \"$descricao\", $status, $id_aviso)'>
                                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='blue' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
                                                 <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/>
                                             </svg>

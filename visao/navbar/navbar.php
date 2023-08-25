4d5fbb7a5f31d62c6d13a2de20f2d773e6e9ec88
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-08-25 12:09:24 -0300

    nome usuário

--- visao/navbar/navbar.php
+++ visao/navbar/navbar.php
@@ -48,6 +48,9 @@
         $arrayDadosFamilia = getDadosFamiliaPerfil($resFamilia); // login/funcoesPHP
         $_SESSION['id_comunidade'] = $arrayDadosFamilia[2];
 
+        $explodeNome = explode(" ", $arrayDadosMembro[0]);
+        // print_r(var_dump($explodeNome[0]));
+
         // $logado = $_SESSION["cpf"];
     }
 
@@ -89,7 +92,7 @@
                 <!---------------------------- campo teste --------------------------------->
                 <li hidden>
                     <a href=""><?php echo "#$codPerfil ";
-                                echo "$arrayDadosMembro[1] - $arrayDadosMembro[2] - $arrayDadosMembro[3]"
+                                echo "$arrayDadosMembro[1] - $arrayDadosMembro[2] - $arrayDadosMembro[3] - $explodeNome[0]"
                                 ?></a>
                 </li>
                 <!-------------------------------------------------------------------------->
@@ -122,7 +125,7 @@
                     <a href="../perfil-fml/">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill mb-1" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                        </svg> <?php echo $arrayDadosMembro[0]; ?>
+                        </svg> <?php echo $explodeNome[0]; ?>
                     </a>
                 </li>
 

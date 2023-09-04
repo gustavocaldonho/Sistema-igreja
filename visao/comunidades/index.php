8a7913671e92df661c0bbd648528dd24ef8d1a73
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-09-03 21:31:40 -0300

    href table tr

--- visao/comunidades/index.php
+++ visao/comunidades/index.php
@@ -83,9 +83,9 @@
 
                         // 
                         while ($user_data = mysqli_fetch_assoc($result)) {
-                            echo "<tr>";
+                            echo "<tr class='nome-comunidade' onclick='redirecionarPerfilComunidade(". $user_data['id_comunidade'] . ")'>";
                             echo "<td>" . $user_data['id_comunidade'] . "</td>";
-                            echo "<td> <a class='btn nome-comunidade' href='../perfil-com/index.php?id_comunidade=" . $user_data['id_comunidade'] . "'> " . $user_data['padroeiro'] . " - " . $user_data['localizacao'] . "</a></td>";
+                            echo "<td>"  . $user_data['padroeiro'] . " - " . $user_data['localizacao'] . "</td>";
                             echo "<td>" . "R$: 0,00" . "</td>";
                             echo "<td>" . "R$: 0,00" . "</td>";
                             // echo "<td>" . $user_data['R$: 0,00'] . "</td>";
@@ -101,7 +101,7 @@
                                 <svg xmlns=1http://www.w3.org/2000/svg1 width='16' height='16' fill='currentColor' class='bi bi-trash3-fill' viewBox='0 0 16 16'>
                                     <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z' />
                                 </svg>
-                            </a>
+                                </a>
                             </td>";
 
                             echo "</tr>";
@@ -141,15 +141,17 @@
     function searchData() {
         window.location = 'index.php?search=' + search.value;
     }
-</script>
 
-<script>
     function setarModalExclusao(id) {
         document.getElementById('btnDeleteComunidadeModal').value = id;
     }
 
     function deleteComunidade(id) {
         window.location.href = "../../controlador/deletarComunidade.php?id_comunidade=" + id;
+    }
+
+    function redirecionarPerfilComunidade(id) {
+        window.location.href = "../perfil-com/index.php?id_comunidade=" + id;
     }
 </script>
 

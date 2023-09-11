7157e0e45779cad228bddf6188868d66c5e54d30
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-09-11 17:35:48 -0300

    order by nome carregarComboMembros

--- dao/membroFamiliaDAO.php
+++ dao/membroFamiliaDAO.php
@@ -3,7 +3,7 @@
 function carregarComboMembros($conexao, $id_comuidade)
 {
     // Variável $sql para criar comando de seleção no banco de dados
-    $sql = "SELECT mf.* FROM bd_sistema.membro_familia mf INNER JOIN bd_sistema.familia f ON mf.id_familia = f.id_familia WHERE f.id_comunidade = '$id_comuidade'";
+    $sql = "SELECT mf.* FROM bd_sistema.membro_familia mf INNER JOIN bd_sistema.familia f ON mf.id_familia = f.id_familia WHERE f.id_comunidade = '$id_comuidade' ORDER BY nome";
 
     // Executando o comando sql no banco de dados, por meio da $conexao
     $res = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));

3f5d2629429d7e5bee0ca4a1a03d2c300ad9a254
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-10-02 11:42:57 -0300

    front radioButton Comunidades

--- visao/comunidades/index.php
+++ visao/comunidades/index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/style-geral.css">
     <link rel="stylesheet" href="../navbar/style-navbar.css">
     <link rel="stylesheet" href="../../visao/comunidades/style-com.css">
@@ -37,7 +38,8 @@
     </div>
 
     <!-- _____________________ modal confirmação exclusão comunidades  _____________________  -->
-    <div class="modal fade" id="modalExclusaoComunidade" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="modalExclusaoComunidade" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -49,7 +51,8 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary" id="btnDeleteComunidadeModal" value="" onclick="deleteComunidade(this.value)">Excluir</button>
+                    <button type="button" class="btn btn-primary" id="btnDeleteComunidadeModal" value=""
+                        onclick="deleteComunidade(this.value)">Excluir</button>
                 </div>
             </div>
         </div>
@@ -57,24 +60,28 @@
     <!--  __________________________________________________________________________________  -->
 
     <div class="box__search">
-        <input type="search" class="form-control w-25" placeholder="Pesquisar comunidade" id="pesquisar" name="pesquisar">
+        <input type="search" class="form-control w-25" placeholder="Pesquisar comunidade" id="pesquisar"
+            name="pesquisar">
         <button onclick="searchData()" class="btn btn-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+                viewBox="0 0 16 16">
+                <path
+                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
         </button>
     </div>
     <div class="box__content-table">
-        <div class="box__principal" data-bs-spy="scroll" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-light p-3 rounded-2" tabindex="0">
+        <div class="box__principal" data-bs-spy="scroll" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true"
+            class="scrollspy-example bg-light p-3 rounded-2" tabindex="0">
             <div>
                 <table class="table table-bg table-hover">
                     <thead>
                         <tr class="sticky-top titulo-table">
-                            <th scope="col" class="col-1">#</th>
+                            <!-- <th scope="col" class="col-1">#</th> -->
                             <th scope="col" class="col-6">Comunidade</th>
                             <th scope="col" class="col-2">Total Arrecadado</th>
                             <th scope="col" class="col-2">Total da Paróquia</th>
-                            <th scope="col" class="col-1">...</th>
+                            <th scope="col" class="col-2">...</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -87,25 +94,31 @@
 
                         // 
                         while ($user_data = mysqli_fetch_assoc($result)) {
-                            echo "<tr class='nome-comunidade'>";
-                            echo "<td onclick='redirecionarPerfilComunidade(" . $user_data['id_comunidade'] . ")'>" . $user_data['id_comunidade'] . "</td>";
+                            echo "<tr id='item-com-" . $user_data['id_comunidade'] . "class='nome-comunidade'>";
+                            // echo "<td onclick='redirecionarPerfilComunidade(" . $user_data['id_comunidade'] . ")'>" . $user_data['id_comunidade'] . "</td>";
                             echo "<td onclick='redirecionarPerfilComunidade(" . $user_data['id_comunidade'] . ")'>"  . $user_data['padroeiro'] . " - " . $user_data['localizacao'] . "</td>";
                             echo "<td onclick='redirecionarPerfilComunidade(" . $user_data['id_comunidade'] . ")'>" . "R$: 0,00" . "</td>";
                             echo "<td onclick='redirecionarPerfilComunidade(" . $user_data['id_comunidade'] . ")'>" . "R$: 0,00" . "</td>";
                             // echo "<td>" . $user_data['R$: 0,00'] . "</td>";
                             // echo "<td>" . $user_data['R$: 0,00'] . "</td>";
                             echo "<td>
-                                <a class='btn btn-sm btn-primary' href='../../controlador/editarComunidade.php?id=$user_data[id_comunidade]'>
-                                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
-                                        <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
-                                    </svg>
-                                </a>
+                                <div class='buttonsComunidade'>
+                                    <a class='btn btn-sm btn-primary' href='../../controlador/editarComunidade.php?id=$user_data[id_comunidade]'>
+                                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                                            <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                                        </svg>
+                                    </a>
 
-                                <a class='btn btn-sm btn-danger' data-bs-toggle='modal' data-bs-target='#modalExclusaoComunidade' onclick='setarModalExclusao($user_data[id_comunidade])'>
-                                <svg xmlns=1http://www.w3.org/2000/svg1 width='16' height='16' fill='currentColor' class='bi bi-trash3-fill' viewBox='0 0 16 16'>
-                                    <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z' />
-                                </svg>
-                                </a>
+                                    <a class='btn btn-sm btn-danger' data-bs-toggle='modal' data-bs-target='#modalExclusaoComunidade' onclick='setarModalExclusao($user_data[id_comunidade])'>
+                                    <svg xmlns=1http://www.w3.org/2000/svg1 width='16' height='16' fill='currentColor' class='bi bi-trash3-fill' viewBox='0 0 16 16'>
+                                        <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z' />
+                                    </svg>
+                                    </a>
+
+                                    <div class='form-check form-switch'>
+                                        <input class='form-check-input' type='checkbox' role='switch' checked id='radioStatusAviso-' onclick=mudarStatus('item-aviso-')>
+                                    </div>
+                                </div>
                             </td>";
 
                             echo "</tr>";
@@ -117,37 +130,40 @@
         </div>
     </div>
     <div class="box_button">
-        <a id="btn-cadComunidade" name="btn-cadComunidade" href="../cad-com/" class="btn btn-primary">Adicionar Comunidade</a>
+        <a id="btn-cadComunidade" name="btn-cadComunidade" href="../cad-com/" class="btn btn-primary">Adicionar
+            Comunidade</a>
     </div>
 
 </body>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+</script>
 
 <script>
-    var search = document.getElementById('pesquisar');
+var search = document.getElementById('pesquisar');
 
-    search.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            searchData();
-        }
-    });
-
-    function searchData() {
-        window.location = 'index.php?search=' + search.value;
+search.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        searchData();
     }
+});
 
-    function setarModalExclusao(id) {
-        document.getElementById('btnDeleteComunidadeModal').value = id;
-    }
+function searchData() {
+    window.location = 'index.php?search=' + search.value;
+}
 
-    function deleteComunidade(id) {
-        window.location.href = "../../controlador/deletarComunidade.php?id_comunidade=" + id;
-    }
+function setarModalExclusao(id) {
+    document.getElementById('btnDeleteComunidadeModal').value = id;
+}
 
-    function redirecionarPerfilComunidade(id) {
-        window.location.href = "../perfil-com/index.php?id_comunidade=" + id;
-    }
+function deleteComunidade(id) {
+    window.location.href = "../../controlador/deletarComunidade.php?id_comunidade=" + id;
+}
+
+function redirecionarPerfilComunidade(id) {
+    window.location.href = "../perfil-com/index.php?id_comunidade=" + id;
+}
 </script>
 
 </html>

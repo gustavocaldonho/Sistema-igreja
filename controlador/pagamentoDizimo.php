c3b0f654f6394d69c11648a26bf3d278ecda4073
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-10-23 16:30:03 -0300

    comentários

--- controlador/pagamentoDizimo.php
+++ controlador/pagamentoDizimo.php
@@ -13,14 +13,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $conexao = conectar();
 
+    // seleciona os pagamentos
     $pagamento = selectPagamento($conexao, $id_familia, $mes);
 
+    // conta a quantidade de pagamentos referentes ao mês em questão (qtd linhas) -> não pode passar de uma
     $contador = 0;
     while ($user_data = mysqli_fetch_assoc($pagamento)) {
         $contador++;
     }
 
+    // verifica se é para inserir um novo pagamento ou alterar o status de um já existente
     if ($cod == 0) {
+        // verifica se já foi feito algum pagamento no referido mês
         if ($contador == 0) {
             inserirPagamento($conexao, $id_pagamento, $id_familia, $mes, $status);
         }

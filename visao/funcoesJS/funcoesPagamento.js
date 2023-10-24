744ed8400f5aa301d2620c62acb2ad96940699f7
Gustavo Caldonho <110199443+gustavocaldonho@users.noreply.github.com>
2023-10-23 21:21:03 -0300

    icons status dizimo

--- visao/funcoesJS/funcoesPagamento.js
+++ visao/funcoesJS/funcoesPagamento.js
@@ -111,16 +111,24 @@ function atualizarStatusPagina(status) {
 
   switch (status) {
     case "0":
-      campoStatus.innerHTML = "Não Pago";
+      campoStatus.innerHTML =
+        "Não Pago" +
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-x-circle-fill ms-2 mb-1' viewBox='0 0 16 16'><path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/></svg>";
       break;
     case "1":
-      campoStatus.innerHTML = "Pago";
+      campoStatus.innerHTML =
+        "Pago" +
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='green' class='bi bi-check-circle-fill ms-2 mb-1' viewBox='0 0 16 16'><path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/></svg>";
       break;
     case "2":
-      campoStatus.innerHTML = "Pendente";
+      campoStatus.innerHTML =
+        "Pendente" +
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='yellow' class='bi bi-exclamation-circle-fill ms-2 mb-1' viewBox='0 0 16 16'><path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z'/></svg>";
       break;
     default:
-      campoStatus.innerHTML = "Não Pago";
+      campoStatus.innerHTML =
+        "Não Pago" +
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-x-circle-fill ms-2 mb-1' viewBox='0 0 16 16'><path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'/></svg>";
       break;
   }
 }
